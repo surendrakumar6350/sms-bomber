19e1233cb665fea622298899cd54ee2b2ede94d5
kali <kamleshksks456@gmail.com>
2025-03-08 09:12:43 +0530

    chore(ci): setup GitHub Actions to run tests before deployment

---
+++ packages/tests/hello.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+
+describe("Mobile Tracking API (/api/hello)", () => {
+    it("should return 400 for invalid mobile number", async () => {
+        jest.setTimeout(20000);
+        const response = await request("http://localhost:3000").get("/api/hello?mobile=123");
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message");
+    }, 50000);
+
+    it("should return success for a valid mobile number", async () => {
+        const response = await request("http://localhost:3000").get("/api/hello?mobile=8234567890");
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("success", true);
+        expect(response.body).toHaveProperty("message", "SMS sent successfully.");
+    }, 50000);
+
+    it("should return 400 if an error occurs", async () => {
+        const response = await request("http://localhost:3000").get("/api/hello");
+
+        expect(response.status).toBe(400);
+    }, 50000);
+});
